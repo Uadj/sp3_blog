69255cfe472c9b1400d88abe9b827508ab5a27d3
dogyuwon <ds257873@naver.com>
2024-03-12 23:36:43 +0900

    Controller

---
+++ src/main/java/com/example/sp3/controller/BlogApiController.java
@@ -0,0 +1,24 @@
+package com.example.sp3.controller;
+
+import com.example.sp3.domain.Article;
+import com.example.sp3.dto.AddArticleRequest;
+import com.example.sp3.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class BlogApiController {
+    private final BlogService blogService;
+
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
+        Article savedArticle = blogService.save(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+    }
+}
