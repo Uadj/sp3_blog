91503976257ab01a34cdc8f102a3e99a18267f2a
dogyuwon <ds257873@naver.com>
2024-03-13 00:56:35 +0900

    Feature - Controller - BlogViewController getArticle(@PathVariable Long id, Model model)

--- src/main/java/com/example/sp3/controller/BlogViewController.java
+++ src/main/java/com/example/sp3/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package com.example.sp3.controller;
 
+import com.example.sp3.domain.Article;
 import com.example.sp3.dto.ArticleListViewResponse;
+import com.example.sp3.dto.ArticleViewResponse;
 import com.example.sp3.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -23,5 +26,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model){
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
