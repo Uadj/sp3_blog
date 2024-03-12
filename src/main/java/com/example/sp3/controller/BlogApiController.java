3869682d11963f9fe8e225ee2ba556060fbdcd58
dogyuwon <ds257873@naver.com>
2024-03-12 23:53:39 +0900

    Feature - Controller - BlogApiController findAllArticles()

--- src/main/java/com/example/sp3/controller/BlogApiController.java
+++ src/main/java/com/example/sp3/controller/BlogApiController.java
@@ -2,13 +2,17 @@ package com.example.sp3.controller;
 
 import com.example.sp3.domain.Article;
 import com.example.sp3.dto.AddArticleRequest;
+import com.example.sp3.dto.ArticleResponse;
 import com.example.sp3.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,5 +24,15 @@ public class BlogApiController {
         Article savedArticle = blogService.save(request);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles(){
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
